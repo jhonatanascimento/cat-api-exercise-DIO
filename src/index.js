764544bfa043f39e4dbda5bc414c27ba1a61b6c9
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2022-01-12 19:51:29 -0300

    changed to pokemon now it generates a sprite image and the name of a random pokemon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const BASE_URL = "https://thatcopy.pw/catapi/rest/";
+let BASE_URL = `https://pokeapi.co/api/v2/pokemon/35/`;
 const catBtn = document.getElementById("change-cat");
+const pokemonName = document.getElementById("pokemon-name");
 const catImg = document.getElementById("cat");
 
 const getCats = async () => {
@@ -7,14 +8,36 @@ const getCats = async () => {
     const data = await fetch(BASE_URL);
     const json = await data.json();
 
-    return json.webpurl;
+    return json.sprites.front_default;
   } catch (e) {
     console.log(e.message);
   }
 };
 
+const getPokemonName = async () => {
+  try {
+    const data = await fetch(BASE_URL);
+    const json = await data.json();
+
+    return json.species.name;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const loadImg = async () => {
+  BASE_URL = `https://pokeapi.co/api/v2/pokemon/${getRandomIntInclusive(
+    1,
+    150
+  )}/`;
   catImg.src = await getCats();
+  pokemonName.textContent = await getPokemonName();
 };
 
 catBtn.addEventListener("click", loadImg);
