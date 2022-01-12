c70e72b62dd06270a2d61f80916fc3a9cb34fc89
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2022-01-12 19:58:43 -0300

    changed id names

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 let BASE_URL = `https://pokeapi.co/api/v2/pokemon/35/`;
-const catBtn = document.getElementById("change-cat");
+const changePokemon = document.getElementById("change-pokemon");
 const pokemonName = document.getElementById("pokemon-name");
-const catImg = document.getElementById("cat");
+const pokemonSprite = document.getElementById("pokemon-sprite");
 
-const getCats = async () => {
+const getPokemonImage = async () => {
   try {
     const data = await fetch(BASE_URL);
     const json = await data.json();
@@ -36,10 +36,10 @@ const loadImg = async () => {
     1,
     150
   )}/`;
-  catImg.src = await getCats();
+  pokemonSprite.src = await getPokemonImage();
   pokemonName.textContent = await getPokemonName();
 };
 
-catBtn.addEventListener("click", loadImg);
+changePokemon.addEventListener("click", loadImg);
 
 loadImg();
